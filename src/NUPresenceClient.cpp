b551a0f815223c06f6d777ef908c044b88aab877
Jake Fountain <jake.fountain@uon.edu.au>
2016-01-08 16:39:08 +1100

    Moved rift code to OVRManager

--- src/NUPresenceClient.cpp
+++ src/NUPresenceClient.cpp
@@ -11,7 +11,7 @@
 
 #include <GL/OOGL.hpp>
 
-#include "OVRManager.h"
+#include "Renderer.h"
 
 int createTCPSocket(uint32_t ip, uint16_t port){
 
@@ -87,47 +87,9 @@ int createUDPSocket(std::string group, uint16_t port, bool multicast){
 }
 
 void riftThreadMain(){
-    bool riftPresent = true;
+	Renderer renderer;
 
-    GL::Window window(800,600,"Visualisation Window", GL::WindowStyle::Close);
-    GL::Context& gl = window.GetContext();
-
-	OVRManager ovrManager;
-
-	// Main loop
-	bool running = true; 
-    while (running)
-    {
-		ovrManager.printCurrentPose();
-    	
-	    GL::Shader vert(GL::ShaderType::Vertex, "#version 150\nin vec2 position; void main() { gl_Position = vec4(position, 0.0, 1.0); }");
-	    GL::Shader frag(GL::ShaderType::Fragment, "#version 150\nout vec4 outColor; void main() { outColor = vec4(1.0, 0.0, 0.0, 1.0); }");
-	    GL::Program program(vert, frag);
-
-		float vertices[] = {	
-	        -0.5f,  0.5f,
-	         0.5f,  0.5f,
-	         0.5f, -0.5f
-	    };
-	    GL::VertexBuffer vbo(vertices, sizeof(vertices), GL::BufferUsage::StaticDraw);
-
-	    GL::VertexArray vao;
-	    vao.BindAttribute(program.GetAttribute("position"), vbo, GL::Type::Float, 2, 0, 0);
-
-	    GL::Event ev;
-	    while (window.IsOpen())
-	    {
-	        while (window.GetEvent(ev));
-
-	        gl.Clear();
-
-	        gl.DrawArrays(vao, GL::Primitive::Triangles, 0, 3);
-
-	        window.Present();
-	    }
-
-    }
-
+	renderer.run();
 }
 
 int main()
